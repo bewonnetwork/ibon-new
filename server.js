3072417733805a2bde5a66697a336ed1f1cf193b
GSN Network <gsnnetwork9@gmail.com>
2026-06-25 13:30:18 +0600

    backend api added

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    status: "running",
+    backend: true,
+    project: "IBON"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
